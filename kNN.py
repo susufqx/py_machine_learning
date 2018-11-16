b2e472751219ac57c22d7aabe59e1bbeaf3b6250
rui.li <jiangsulirui@gmail.com>
2018-11-16 16:04:41 +0800

    Add test function

--- kNN.py
+++ kNN.py
@@ -1,26 +1,36 @@
-from numpy import *
 import operator
+from numpy import *
+
 
 def createDataSet():
     group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
 
+
 def classify0(inX, dataSet, labels, k):
     # 计算距离
     dataSetSize = dataSet.shape[0]  # shape 矩阵的维数, shape[0]就是矩阵的行数
-    diffMat = tile(inX, (dataSetSize, 1)) - dataSet # 形成纵向的许多inX,然后和原始的训练样本求差
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet  # 形成纵向的许多inX,然后和原始的训练样本求差
     sqDiffMat = diffMat ** 2
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances ** 0.5
 
-    sortedDistIndicies = distances.argsort() #从小到大的下标
-    classCount={}
-    for i in range(k): # 对于取的k个数
-        voteIlabel = labels[sortedDistIndicies[i]] # 对应的label
-        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1  # 给classCount赋值，并且计算每个label的出现次数
-    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)  # 通过classCount.iteritems()里每一个的第二个维度排序
+    sortedDistIndicies = distances.argsort()  # 从小到大的下标
+    classCount = {}
+    for i in range(k):  # 对于取的k个数
+        voteIlabel = labels[sortedDistIndicies[i]]  # 对应的label
+        classCount[voteIlabel] = classCount.get(
+            voteIlabel, 0) + 1  # 给classCount赋值，并且计算每个label的出现次数
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(
+        1), reverse=True)  # 通过classCount.iteritems()里每一个的第二个维度排序
     return sortedClassCount[0][0]
+
+
+''' 
+读取文件并且转换为python支持的格式输出
+'''
+
 
 def file2matrix(filename):
     fr = open(filename)
@@ -36,3 +46,42 @@ def file2matrix(filename):
         classLabelVector.append(int(listFromLine[-1]))
         index += 1
     return returnMat, classLabelVector
+
+
+'''
+归一转化
+'''
+
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)  # 每一列的最小值
+    maxVals = dataSet.max(0)  # 每一列的最大值
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet / tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+
+'''
+测试代码
+'''
+
+
+def datingClassTest():
+    hoRatio = 0.10  # 因为测试数据取十分之一，所以是0.1
+    datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :],  # 此处normMat[numTestVecs:m, :]表示另外那90%的样本矩阵
+                                     datingLabels[numTestVecs:m, 3])  # 此处normMat[numTestVecs:m, 3] 90%的样本矩阵的第四列，也就是标签
+        print "the classifier came back with: %d, the real answer is: %d" \
+            % (classifierResult, datingLabels[i])
+
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1.0
+    print "the total error rate is: %f" % (errorCount/float(numTestVecs))
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import kNN
 import matplotlib.pyplot as plt
+from numpy import array
 
 datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
 print('DATING MATRIX: ', datingDataMat)
@@ -7,5 +8,6 @@ print('DATING LABELS: ', datingLabels[0:20])
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2])
+ax.scatter(datingDataMat[:, 0], datingDataMat[:, 1],
+           15.0 * array(datingLabels), 15.0 * array(datingLabels))
 plt.show()
